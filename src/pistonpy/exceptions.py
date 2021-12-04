88b6133ea4d39fcd9afd6f043a89e82f0a3e91cd
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2021-12-04 17:51:00 +0530

    Custom error messages

---
+++ src/pistonpy/exceptions.py
@@ -0,0 +1,19 @@
+# Coding=UTF8
+# !python
+# !/usr/bin/env python3
+
+class CodeNotFound(Exception):
+    pass
+
+class LanguageNotFound(Exception):
+    pass
+
+class CodeFormatNotFound(Exception):
+    pass
+
+class NotAFile(Exception):
+    pass
+
+
+class PistonError(Exception):
+    pass
