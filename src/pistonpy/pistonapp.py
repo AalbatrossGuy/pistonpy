cb9c024f9f2c063d19e826b3d43c150f9d38ba02
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2021-12-04 20:20:13 +0530

    Added safety checks

--- src/pistonpy/pistonapp.py
+++ src/pistonpy/pistonapp.py
@@ -1,7 +1,10 @@
 # Coding=UTF8
 # !python
 # !/usr/bin/env python3
-import requests
+import requests, json
+from typing import Optional
+from .exceptions import CodeNotFound, LanguageNotFound, CodeFormatNotFound, NotAFile
+from .models import GetOutput
 
 __all__ = ('PistonApp',)
 
@@ -16,8 +19,80 @@ class PistonApp():
         self.embed = embed
         self._endpoint = "https://emkc.org/api/v2/piston"
 
+    def __repr__(self) -> str:
+        return "<pistonpy.PistonApp>"
+
     @property
     def runtimes(self) -> dict:
         url = self._endpoint + "/runtimes"
         response = requests.request("GET", url)
-        return response
+        data = response.json()
+        return { item.pop("language"): item for item in data }
+
+
+    @property
+    def languages(self) -> list:
+        url = self._endpoint + "/runtimes"
+        response = requests.request("GET", url)
+        data = response.json()
+        lang = []
+        for language in data:
+            lang.append(language['language'])
+        return lang
+
+    def run(
+        self,
+        language: str,
+        version: str = "*",
+        files: Optional[list] = [],
+        code: Optional[str] = "",
+        args: Optional[list] = [],
+        input: Optional[str] = "",
+        compile_timeout: Optional[int] = 10_000,
+        run_timeout: Optional[int] = 3_000,
+        compile_memory_limit: Optional[int] = -1,
+        run_memory_limit: Optional[int] = -1,
+    ) -> list:
+
+
+        """Main Code Execution"""
+        main_code = ''
+        if not code and not files:
+            print('running CodeNotFound')
+            raise CodeNotFound("No code provided to run")
+
+        elif code and files:
+            print('running CodeFormatNotFound')
+            raise CodeFormatNotFound("Cannot choose whether to run raw code or code from file/s")
+
+        else:
+            if code:
+
+                main_code = [{"name" : '', "content" : code}]
+
+            if files:
+                files_content = []
+
+                for file in files:
+                    try:
+                        with open(file, mode="r") as f:
+                            content = f.read()
+                            files_content.append({"name": file, "content": content})
+                    except FileNotFoundError:
+                        raise NotAFile(f"{file} is not a file object.")
+                main_code = files_content
+
+
+        payload = {
+            'language' : language,
+            'version' : version,
+            'files' : main_code,
+            'args' : args,
+            'stdin' : input,
+            'compile_timeout' : compile_timeout,
+            'run_timeout' : run_timeout,
+            'compile_memory_limit' : compile_memory_limit,
+            'run_memory_limit' : run_memory_limit
+        }
+
+        return GetOutput(payload).parse_output()
