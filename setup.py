6a8eb128efa38fe2480636f2725b8146abac44d5
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2021-12-04 20:27:42 +0530

    v0.0.2 released

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", mode="r", encoding="utf-8") as file:
 
 setup(
     name="pistonpy",
-    version="0.0.1",
+    version="0.0.2",
     description="An unofficial wrapper for Engineer Man's Piston API",
     long_description=long_desc,
     long_description_content_type="text/markdown",
