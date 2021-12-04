1bdcb3aebe5b9b64c8c4d9ea545631a112d92437
AaalbatrossGuy <thexcelsiorisback@gmail.com>
2021-12-04 20:19:34 +0530

    Added PistonApp() examples

--- examples/app.py
+++ examples/app.py
@@ -1,1 +1,21 @@
 from pistonpy import PistonApp
+import requests, json
+
+# Initialize the client.
+piston = PistonApp()
+
+my_code = "print('This code ran from app.py itself!')"
+
+output = piston.run(language="python", version="3.10.0", code=my_code)
+
+print(output)
+
+# Output: {'language': 'python', 'version': '3.10.0', 'run': {'stdout': 'This code ran from app.py itself!\n', 'stderr': '', 'code': 0, 'signal': None, 'output': 'This code ran from app.py itself!\n'}}
+
+run_file = piston.run(language="python", files=['test.py']) # version is optional. files even if it maybe only one must be given as lists.
+
+print(run_file)
+
+# Output: {'language': 'python', 'version': '3.10.0', 'run': {'stdout': 'The code ran from test.py\n', 'stderr': '', 'code': 0, 'signal': None, 'output': 'The code ran from test.py\n'}}
+
+# You cannot provide both code and files in a single PistonAPP().run() instance.
